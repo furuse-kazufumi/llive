b9042b4c24d00386b018e79302d1e9037e386f66
furuse-kazufumi <kazufumi@furuse.work>
2026-05-14 22:31:18 +0900

    auto: Cargo.toml 編集前 (2026-05-14 22:31)

---
+++ crates/llive_rust_ext/target/release/deps/pyo3_build_config-ade0cd7e066273b8.d
@@ -0,0 +1,14 @@
+D:\projects\llive\crates\llive_rust_ext\target\release\deps\pyo3_build_config-ade0cd7e066273b8.d: C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\lib.rs C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\errors.rs C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\impl_.rs D:\projects\llive\crates\llive_rust_ext\target\release\build\pyo3-build-config-a0fab29371f27f86\out/pyo3-build-config-file.txt D:\projects\llive\crates\llive_rust_ext\target\release\build\pyo3-build-config-a0fab29371f27f86\out/pyo3-build-config.txt
+
+D:\projects\llive\crates\llive_rust_ext\target\release\deps\libpyo3_build_config-ade0cd7e066273b8.rlib: C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\lib.rs C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\errors.rs C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\impl_.rs D:\projects\llive\crates\llive_rust_ext\target\release\build\pyo3-build-config-a0fab29371f27f86\out/pyo3-build-config-file.txt D:\projects\llive\crates\llive_rust_ext\target\release\build\pyo3-build-config-a0fab29371f27f86\out/pyo3-build-config.txt
+
+D:\projects\llive\crates\llive_rust_ext\target\release\deps\libpyo3_build_config-ade0cd7e066273b8.rmeta: C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\lib.rs C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\errors.rs C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\impl_.rs D:\projects\llive\crates\llive_rust_ext\target\release\build\pyo3-build-config-a0fab29371f27f86\out/pyo3-build-config-file.txt D:\projects\llive\crates\llive_rust_ext\target\release\build\pyo3-build-config-a0fab29371f27f86\out/pyo3-build-config.txt
+
+C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\lib.rs:
+C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\errors.rs:
+C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\impl_.rs:
+D:\projects\llive\crates\llive_rust_ext\target\release\build\pyo3-build-config-a0fab29371f27f86\out/pyo3-build-config-file.txt:
+D:\projects\llive\crates\llive_rust_ext\target\release\build\pyo3-build-config-a0fab29371f27f86\out/pyo3-build-config.txt:
+
+# env-dep:CARGO_PKG_VERSION=0.24.2
+# env-dep:OUT_DIR=D:\\projects\\llive\\crates\\llive_rust_ext\\target\\release\\build\\pyo3-build-config-a0fab29371f27f86\\out
---
+++ crates/llive_rust_ext/target/release/deps/pyo3_macros-b3e7bcfd1609ef8b.d
@@ -0,0 +1,7 @@
+D:\projects\llive\crates\llive_rust_ext\target\release\deps\pyo3_macros-b3e7bcfd1609ef8b.d: C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-macros-0.24.2\src\lib.rs
+
+D:\projects\llive\crates\llive_rust_ext\target\release\deps\pyo3_macros-b3e7bcfd1609ef8b.dll: C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-macros-0.24.2\src\lib.rs
+
+C:\Users\puruy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-macros-0.24.2\src\lib.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.24.2
