40a26bf44c9a0e808227b175140107658a02b850
furuse-kazufumi <kazufumi@furuse.work>
2026-05-22 23:24:37 +0900

    auto: __init__.py 編集前 (2026-05-22 23:24)

--- src/llive/perf/evolutionary/__init__.py
+++ src/llive/perf/evolutionary/__init__.py
@@ -73,6 +73,11 @@ from llive.perf.evolutionary.frozen_gene import (
 )
 from llive.perf.evolutionary.frozen_registry import FrozenGeneRegistry
 from llive.perf.evolutionary.genome import Genome, GenomeBounds
+from llive.perf.evolutionary.genome_3d import (
+    Genome3D,
+    cross_layer_crossover,
+    intra_layer_crossover,
+)
 from llive.perf.evolutionary.impl_chromosome import (
     KNOWN_IMPL_ALGORITHM_FAMILIES,
     KNOWN_IMPL_JUDGE_MODELS,
